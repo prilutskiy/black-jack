eae582d611242db37e07deaa998ad113a75e1270
Max Prilutskiy <maks.prilutskiy@gmail.com>
2015-04-06 21:59:40 +0300

    Common lib filled with skeleton types

---
+++ BlackJack.Common/CardFactory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJack.Common
+{
+    public static class CardFactory
+    {
+        private static Random rand = new Random();
+        public static Card SpawnCard()
+        {
+            int suit = rand.Next(1, 4);
+            int value = rand.Next(1, 13);
+            var c = new Card(suit, value);
+            return c;
+        }
+    }
+}
